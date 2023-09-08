6d4f8e6144999380b392fb24df4a2977eb84bc7e
Veerasaroot Bunkangwan <bosunakakawa@gmail.com>
2023-09-08 22:32:59 +0700

    Add src

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Font Awesome
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faArrowRightToBracket } from '@fortawesome/free-solid-svg-icons'
+import { faBook } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faArrowRightToBracket, faBook)
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
